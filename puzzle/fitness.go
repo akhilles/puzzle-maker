0d7a4f20a191237b6c3713de84b81d6e324b168a
akhilles <akhilvelagapudi@gmail.com>
2017-09-17 12:51:38 -0400

    Fitness outline

---
+++ puzzle/fitness.go
@@ -0,0 +1,12 @@
+package puzzle
+
+// Evaluate returns the fitness score of a puzzle.
+func Evaluate(p []int) int {
+	return 0
+}
+
+// BFS returns a matrix containing the minimum number of moves needed to reach that cell from the start cell.
+func BFS(p []int) []int {
+	minDist := make([]int, len(p)+1)
+	return minDist
+}
