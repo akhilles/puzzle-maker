c39403a40ac6ce48b2bd6eb3ec0d5d3c6f57127f
akhilles <akhilvelagapudi@gmail.com>
2017-09-17 18:12:30 -0400

    Init minDist with high vals

--- puzzle/fitness.go
+++ puzzle/fitness.go
@@ -6,7 +6,12 @@ func Evaluate(p []int) int {
 }
 
 // BFS returns a matrix containing the minimum number of moves needed to reach that cell from the start cell.
-func BFS(p []int) []int {
-	minDist := make([]int, len(p)+1)
+func BFS(n int, p []int) []int {
+	numCells := n * n
+	minDist := make([]int, numCells)
+	for index := range minDist {
+		minDist[index] = numCells
+	}
+
 	return minDist
 }
