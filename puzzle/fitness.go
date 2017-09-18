2493beb0e07829f7d876fc1b6edaec60cb11b06b
akhilles <akhilvelagapudi@gmail.com>
2017-09-18 18:17:58 -0400

    Ported BFS to Go

--- puzzle/fitness.go
+++ puzzle/fitness.go
@@ -6,12 +6,30 @@ func Evaluate(p []int) int {
 }
 
 // BFS returns a matrix containing the minimum number of moves needed to reach that cell from the start cell.
-func BFS(n int, p []int) []int {
-	numCells := n * n
-	minDist := make([]int, numCells)
-	for index := range minDist {
-		minDist[index] = numCells
+func BFS(n int, p []int, vm [][]int) []int {
+	minDist := make([]int, n*n)
+	q := []int{0}
+	accum, depth, nodes := 0, 1, 1
+
+	for len(q) > 0 {
+		loc := q[0]
+		q = q[1:]
+		for _, move := range vm[loc] {
+			if minDist[move] > depth || (minDist[move] == 0 && move != 0) {
+				minDist[move] = depth
+				q = append(q, move)
+				accum++
+			}
+		}
+
+		nodes--
+		if nodes == 0 {
+			depth++
+			nodes = accum
+			accum = 0
+		}
 	}
 
+	PrintPuzzle(n, minDist)
 	return minDist
 }
