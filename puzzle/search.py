4ed1d06847f8fa401510aed93cdd7c97d52ecb4c
nithint29 <nithint29@gmail.com>
2017-09-26 02:57:38 -0400

    minor changes

--- puzzle/search.py
+++ puzzle/search.py
@@ -1,6 +1,11 @@
 import numpy as np
 from evaluate import *
 import matplotlib.pyplot as plt
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
 
 def hill_climbing(n = 5, iters = 500, report = 100, p=0, temperature = 0, decay = 0.9):
     """returns the resultant puzzle, its evaluation, and the iteration data.
@@ -16,6 +21,7 @@ def hill_climbing(n = 5, iters = 500, report = 100, p=0, temperature = 0, decay 
         index = np.random.randint(0,n*n-1)
         r = index/n
         c = index%n
+        walk_chance = np.random.uniform(0, 1)
 
         _, fitness = evaluate(puzzle)
         curr = puzzle[r,c]
@@ -59,6 +65,21 @@ def hill_restarts(n = 5, restarts = 10, iters = 100, report = 100, p=0, temperat
 
     return best_puzzle, best_eval, np.array(data)
 
+@app.route('/climb')
+@cross_origin()
+def climbing(n=5):
+    iters = 5000
+    puz, best, tempdata = hill_restarts(n, restarts=1, iters=iters, report=100, p=0.2, temperature=0, decay=0.999)
+    depth,b = evaluate(puz)
+    puz = puz.flatten()
+    depth = depth.flatten()
+
+    return jsonify(
+        Cells=list(puz),
+        DepthBFS=list(depth),
+        fitness=best,
+        Iterations=iters
+    )
 
 def rand_value(loc, n):
     r,c = loc[0]+1,loc[1]+1
@@ -83,63 +104,72 @@ if __name__=='__main__':
     # print(evaluate(puz))
     # print(hill_climbing(puz, 1))
 
-    sizes = [5,7,9,11]
-    num_runs = 50
+    # sizes = [5,7,9,11]
+    # num_runs = 50
+    #
+    # iters = 1000
+    # report = 50
+    #
+    # for n in sizes:
+    #     data = np.zeros(int(iters/report))
+    #
+    #     for i in range(num_runs):
+    #         puz, best, tempdata = hill_restarts(n,restarts=1,iters=iters,report=report, p= 0.2, temperature = 0, decay=0.999)
+    #         print("Restarts: ", puz, best)
+    #         data = data + tempdata
+    #
+    #     plt.figure()
+    #     plt.plot(np.arange(0,iters,report),data/num_runs)
+    #     plt.ylabel('Fitness')
+    #     plt.xlabel('Iterations')
+    #     plt.title('{} by {} HC with Random Walk'.format(n,n))
+    #     plt.savefig('plots/{}by{}rwalk.png'.format(n,n))
 
-    iters = 1000
-    report = 50
 
-    for n in sizes:
-        data = np.zeros(int(iters/report))
-
-        for i in range(num_runs):
-            puz, best, tempdata = hill_restarts(n,restarts=1,iters=iters,report=report, p= 0.2, temperature = 0, decay=0.999)
-            print("Restarts: ", puz, best)
-            data = data + tempdata
-
-        plt.figure()
-        plt.plot(np.arange(0,iters,report),data/num_runs)
-        plt.ylabel('Fitness')
-        plt.xlabel('Iterations')
-        plt.title('{} by {} HC with Random Walk'.format(n,n))
-        plt.savefig('plots/{}by{}rwalk.png'.format(n,n))
-
-
-    puz, best, data = hill_restarts(11, restarts=2, iters=10000, report=100, p=0.2, temperature=0, decay=0)
-    print("Basic: ", puz, best)
-    plt.figure()
-    plt.plot(np.arange(0, 10000, 100), data)
-    plt.ylabel('Fitness')
-    plt.xlabel('Iterations')
-    plt.title('{} by {} HC with Random Walk'.format(11, 11))
-    plt.savefig('plots/{}by{}rwalkdeep.png'.format(11, 11))
+    # puz, best, data = hill_restarts(11, restarts=1, iters=10000, report=100, p=0.2, temperature=0, decay=0)
+    # print("Basic: ", puz, best)
+    # plt.figure()
+    # plt.plot(np.arange(0, 10000, 100), data)
+    # plt.ylabel('Fitness')
+    # plt.xlabel('Iterations')
+    # plt.title('{} by {} HC with Random Walk'.format(11, 11))
+    # plt.savefig('plots/{}by{}rwalkdeep.png'.format(11, 11))
 
 
 
     #try different params
 
-    # ps = np.array([0.05,.1,.15,.2,0.25,.3])
-    #
-    # bestp = 0
-    # bestval = -1
-    # pgraph = []
-    # for p in ps:
-    #     data = np.zeros(30)
-    #     for i in range(2):
-    #         puz, best, datatemp = hill_restarts(11, restarts=1, iters=3000, report=100, p=p, temperature=0, decay=0.999)
-    #         data += datatemp
-    #     data = data/2
-    #     pgraph.append(data[-1])
-    #     if(data[-1]>bestval):
-    #         bestp = p
-    #         bestval = data[-1]
-    #
-    # print(bestp,bestval)
-    #
+    ps = np.array([.02,.01,.005,.002,.001,.0005])
+
+    bestp = 0
+    bestval = -1
+    pgraph = []
+    for p in ps:
+        data = np.zeros(30)
+        for i in range(2):
+            puz, best, datatemp = hill_restarts(11, restarts=1, iters=3000, report=100, p=p, temperature=0, decay=0.999)
+            data += datatemp
+        data = data/2
+        pgraph.append(data[-1])
+        if(data[-1]>bestval):
+            bestp = p
+            bestval = data[-1]
+
+    print(bestp,bestval)
+
+    plt.figure()
+    plt.plot(ps,np.array(pgraph))
+    plt.ylabel('Average Evaluation')
+    plt.xlabel('Value of p')
+    plt.title('Determining goo value for p'.format(11, 11))
+    plt.savefig('plots/pgraph')
+
+    # puz, best, data = hill_restarts(11, restarts=1, iters=10000, report=100, p=0, temperature=100, decay=0.999)
+    # print("Basic: ", puz, best)
     # plt.figure()
-    # plt.plot(ps,np.array(pgraph))
-    # plt.ylabel('Average Evaluation')
-    # plt.xlabel('Value of p')
-    # plt.title('Determining goo value for p'.format(11, 11))
-    # plt.savefig('plots/pgraph')
+    # plt.plot(np.arange(0, 10000, 100), data)
+    # plt.ylabel('Fitness')
+    # plt.xlabel('Iterations')
+    # plt.title('{} by {} HC with Annealing'.format(11, 11))
+    # plt.savefig('plots/{}by{}annealdeep.png'.format(11, 11))
 
